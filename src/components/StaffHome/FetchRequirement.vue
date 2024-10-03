bd27e4f0dedc915c00b5f97d2d382264048e87d3
code-with-sina <sina.dev.engr@gmail.com>
2024-10-03 19:35:05 +0100

    just a little change

--- src/components/StaffHome/FetchRequirement.vue
+++ src/components/StaffHome/FetchRequirement.vue
@@ -30,16 +30,22 @@ import { ref } from 'vue';
     fetchRequirements();
 </script>
 <template>
-    <Panel v-for="item in requirements" v-bind:key="item.id"  collapsed :pt="{ root: { class: 'text-xs px-10 my-2'}, togglerIcon: { class: 'w-2'}, header: { class: 'rounded py-1' } }">
-        <template #header>
-            <div class="flex flex-row pt-2 w-full">
-                <span  class="basis-5/6 font-bold">{{ item.requirement }}</span>
-                <span  class="basis-1/6 float-right font-normal">
-                    
-                    {{ item.status }}
-                </span>
-            </div>
-        </template>
-    </Panel>
-
+     <Card :pt="{root: { class: 'mx-1 text-xs my-2 py-0 mx-0 px-0 shadow-none bg-transparent' }, content: { class: 'my-1 mx-0 py-1 px-1'}  }">
+        <template #content>
+            <Panel v-for="item in requirements" v-bind:key="item.id"  collapsed :pt="{ root: { class: 'text-xs px-10 my-2'}, togglerIcon: { class: 'w-2'}, header: { class: 'rounded py-1' } }">
+                <template #header>
+                    <div class="flex flex-row pt-2 w-full">
+                        <span  class="basis-5/6 font-bold">{{ item.requirement }}</span>
+                        <span  class="basis-1/6 float-right font-normal">
+                            {{ item.status }}
+                        </span>
+                        <span  class="basis-1/6 float-right font-normal">
+                            <i class="pi pi-align-justify" style="color: slateblue"></i>
+                            
+                        </span>
+                    </div>
+                </template>
+            </Panel>
+        </template> 
+    </Card>
 </template>
